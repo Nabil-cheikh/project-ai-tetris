5709c8a43c8ec708f81c86ceb18b49e243d8eff7
Marc Fernandes <marc.fernandes95@gmail.com>
2024-08-20 01:18:51 +0200

    Add new variables on params.py

--- IA_Tetris/params.py
+++ IA_Tetris/params.py
@@ -1,3 +1,29 @@
 import os
 
+
+############ VARIABLES ############
 ROM_PATH = os.environ.get('ROM_PATH')
+MODEL_TARGET = os.environ.get('MODEL_TARGET')
+
+# GCP Project
+GCP_PROJECT = os.environ.get('GCP_PROJECT')
+GCP_REGION = os.environ.get('GCP_REGION')
+
+# Cloud Storage
+BUCKET_NAME = os.environ.get('BUCKET_NAME')
+
+# BigQuery
+BQ_REGION = os.environ.get('BQ_REGION')
+BQ_DATASET = os.environ.get('BQ_DATASET')
+
+
+############ CONSTANTS ############
+COLUMN_NAMES = ['Time', 'Score', 'Lines', 'Rewards', 'NbBlocUsed']
+
+DTYPES = {
+    'Time': 'timestamp',
+    'Score': 'int',
+    'Lines': 'int',
+    'Rewards': 'int',
+    'NbBlocUsed': 'int'
+}
