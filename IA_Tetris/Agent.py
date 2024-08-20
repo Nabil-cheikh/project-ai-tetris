23f8c3288ab602da9ca863bcda8020897babbea0
Nabil Cheikh <nabil.cheikh13@hotmail.com>
2024-08-20 10:46:20 +0200

    Add Agent

---
+++ IA_Tetris/Agent.py
@@ -0,0 +1,32 @@
+import keras
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+class TetrisAgent:
+
+    def __init__(self, is_eval=False, model_name=""):
+        self.action_size = 3 # sit, buy, sell
+        self.memory = deque(maxlen=1000)
+        self.inventory = []
+        self.model_name = model_name
+        self.is_eval = is_eval
+
+        self.gamma = 0.95
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+
+        self.model = self._build_model()
+
+
+    def _build_model(self):
+        model = Sequential()
+        model.add(Dense(units=64, input_dim=self.state_size, activation="relu"))
+        model.add(Dense(units=32, activation="relu"))
+        model.add(Dense(units=8, activation="relu"))
+        model.add(Dense(self.action_size, activation="linear"))
+        model.compile(loss="mse", optimizer=Adam(lr=0.001))
+
+        return model
